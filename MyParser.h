c96098ab5ecdc47eb31758c6fb0e5ef25d41bbf1
Jan Kašpar <jan.kaspar@cern.ch>
2016-09-01 13:19:10 +0200

    Removed unneeded MyException.h.

--- MyParser.h
+++ MyParser.h
@@ -14,61 +14,50 @@
 #include <string>
 #include <vector>
 
-#include "MyException.h"
 
 using namespace std;
-
-// TODO: remove
-void LineParser(ifstream&, vector<string>&);
-void CSVLinesParser(vector<string>, vector<vector<string> >&);
 
 /// Function to turn a multiple lines file into a vector of strings 
 void LineParser(ifstream& file_to_parse, vector<string>& lines)
 {
-  try
-  { 
-    if (!file_to_parse)
+	if (!file_to_parse)
 	{
-      throw MyException("ERROR: file to parse not found!");
-      goto END;     
-    }
-    
-    while (true)
+		printf("ERROR in LineParser > file not found.\n");
+		exit(1);
+	}
+	
+	while (true)
 	{
-      if (file_to_parse.eof())
-	  	break;
-      string line;
-      getline(file_to_parse,line);
-      lines.push_back(line);
-    }
-    
-    END:;
-  }
+		if (file_to_parse.eof())
+			break;
 
-  catch(MyException& caught)
-  {
-    std::cout << caught.what() << endl;
-  }
-  
+		string line;
+		getline(file_to_parse, line);
+		lines.push_back(line);
+	}
 }
 
 /// Function to turn a vector of lines coming from a .csv in a vector
 /// of vectors containing the lables
-void CSVLinesParser(vector<string> lines, vector<vector<string> >& spreadsheet)
+void CSVLinesParser(vector<string> lines, vector<vector<string>> &spreadsheet)
 {
-  for (vector<string>::iterator line=lines.begin();line!=lines.end();++line)
-  {
-    string field_content;
-    istringstream linestream(*line);
-    if (!linestream.eof()){
-      spreadsheet.push_back(vector<string>());
-      while(1){
-	getline(linestream,field_content,',');
-	spreadsheet.back().push_back(field_content);
-	if (linestream.eof()){break;}
-      }
-    }
-  }
+	for (vector<string>::iterator line=lines.begin(); line!=lines.end(); ++line)
+	{
+		string field_content;
+		istringstream linestream(*line);
+		if (!linestream.eof())
+		{
+			spreadsheet.push_back(vector<string>());
+	
+			while(1)
+			{
+				getline(linestream,field_content,',');
+				spreadsheet.back().push_back(field_content);
+				if (linestream.eof())
+					break;
+			}
+		}
+	}
 }
 
 #endif
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all: RDAnalysis
 
-RDAnalysis: RDAnalysis.cc MyException.h MyParser.h
+RDAnalysis: RDAnalysis.cc MyParser.h
 	g++ `root-config --libs` -lMinuit `root-config --cflags` --std=c++11 -g \
 		RDAnalysis.cc -o RDAnalysis
